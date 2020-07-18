e76ca9d93ecba450c1b0c3eb2111c1a898dc1d14
jprochazk <honza.spacir@gmail.com>
2020-07-18 10:33:43 +0200

    added build.sh

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+usage()
+{
+    echo "usage: build [-t | --type [debug|release]] [-h | --help]"
+    echo "example: build --type release"
+    exit 
+}
+
+build_type=debug
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -t | --type)    shift 
+                        build_type=$1 
+                        ;;
+        -h | --help)    usage 
+                        ;;
+    esac
+    shift
+done
+
+if [ ! -d build ]; then
+    mkdir build
+fi
+
+case "$build_type" in
+    debug)      cd build && cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug && make -j 8 ;;
+    release)    cd build && cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release && make -j 8 ;;
+    *)          echo "invalid build type: $build_type"
+                echo "must be one of: debug, release"
+                ;;
+esac
